ea2fcb062137c8564e884d9a98f37e2cc9f0cec0
dallas marlow <dallasmarlow@gmail.com>
2012-05-14 15:24:18 -0400

    byte to unit conversion

--- lib/kibi/kibi.rb
+++ lib/kibi/kibi.rb
@@ -11,6 +11,10 @@ module Kibi
         [bytes.to_f / yobibyte, :yb]
       when bytes >= zebibyte
         [bytes.to_f / zebibyte, :zb]
+      when bytes >= exbibyte
+        [bytes.to_f / exbibyte, :eb]
+      when bytes >= pebibyte
+        [bytes.to_f / pebibyte, :pb]
       when bytes >= tebibyte
         [bytes.to_f / tebibyte, :tb]
       when bytes >= gibibyte
@@ -31,5 +35,27 @@ module Kibi
 
       humanize p.size
     end
+
+    def bytes_to bytes, unit
+      case unit
+      when :k, :kb, :kibibytes, :kilobytes
+        bytes.to_f / kibibyte
+      when :m, :mb, :mebibytes, :megabytes
+        bytes.to_f / mebibyte
+      when :g, :gb, :gibibytes, :gigabytes
+        bytes.to_f / gibibyte
+      when :t, :tb, :tebibytes, :terabytes
+        bytes.to_f / tebibyte
+      when :p, :pb, :pebibytes, :petabytes
+        bytes.to_f / pebibyte
+      when :e, :eb, :exbibytes, :exabytes
+        bytes.to_f / exbibyte
+      when :z, :zb, :zebibytes, :zettabytes
+        bytes.to_f / zebibyte
+      when :y, :yb, :yobibytes, :yottabytes
+        bytes.to_f / yobibyte
+      end
+    end
+
   end
 end
